cfbc476bc44194a592c84d388bc7dc36ae45ef9d
seenso <>
2021-10-04 10:08:30 -0800

    Completed assignment - just looked over tests, didn't change anything

--- test/index-test.js
+++ test/index-test.js
@@ -1,14 +1,12 @@
 const { name, height, message } = require("../index.js");
 
-/*
 describe('what-is-a-test', () => {
   describe('Name', () => {
     it('returns "Susan"', () => {
       expect(name).toEqual('Susan')
     })
   })
-
-
+  
   describe('Height', () => {
     it('is less than 40', () => {
       expect(height).toBeLessThan(40)
@@ -22,4 +20,4 @@ describe('what-is-a-test', () => {
     })
   })
 })
-*/
+
